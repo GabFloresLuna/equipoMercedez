31952698e23be3a6f8f5be83ebdd486e6eeab8e6
GabFloresLuna <gab.floresl@duocuc.cl>
2026-04-28 10:47:35 -0400

    Inicialización de proyectos

---
+++ repuestosEquipoMercedez/src/main/java/cl/duoc/repuestosEquipoMercedez/RepuestosEquipoMercedezApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.repuestosEquipoMercedez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosEquipoMercedezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosEquipoMercedezApplication.class, args);
+	}
+
+}
---
+++ vehiculosEquipoMercedez/src/test/java/cl/duoc/vehiculosEquipoMercedez/VehiculosEquipoMercedezApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.vehiculosEquipoMercedez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosEquipoMercedezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
